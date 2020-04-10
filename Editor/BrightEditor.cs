16272ba0bc770c2c61be591f87144341582d46dc
Silvio Carrera <carrera.silvio@gmail.com>
2020-04-10 17:12:26 -0400

    Extracted first scripts with editor extensions

---
+++ Editor/BrightEditor.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BrightLib.BrightEditor.Core
+{
+    /// <summary>
+	/// Derive from this class to create a custom inspector or editor for your object. With quality-of-life methods.
+	/// </summary>
+    public class BrightEditor : UnityEditor.Editor
+    {
+        
+    }
+}
---
+++ Editor/BrightEditorGUILayout.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+namespace BrightLib.BrightEditor.Core
+{
+	public static class BrightEditorGUILayout
+	{
+		public static void LabelFieldBold(string text)
+		{
+			EditorGUILayout.LabelField(text, EditorStyles.boldLabel);
+		}
+
+		public static void DrawProperty(SerializedObject serializedObject, string propertyName)
+		{
+			SerializedProperty property = serializedObject.FindProperty(propertyName);
+			EditorGUILayout.PropertyField(property, true);
+		}
+
+		public static void DrawProperty(SerializedProperty property)
+		{
+			EditorGUILayout.PropertyField(property, true);
+		}
+	}
+}
+
