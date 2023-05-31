09aabb09c49a678ebd1f1caa9ef7afe83f89a8c6
carreraSilvio <carrera.silvio@gmail.com>
2023-05-31 17:15:36 +0100

    Added method to handle array updated old methods

--- Editor/BrightEditorPrefs.cs
+++ Editor/BrightEditorPrefs.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Text;
+using UnityEditor;
 using UnityEngine;
 
 namespace BrightTooling
@@ -10,12 +12,61 @@ namespace BrightTooling
     {
         private BrightEditorPrefs() { }
 
-        public void SetObject(string key, object value)
+        private static string ConvertToString<T>(T[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(", ");
+                }
+
+                sb.Append(array[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static void SetColor(string key, Color color)
+        {
+            EditorPrefs.SetFloat($"{key}_r", color.r);
+            EditorPrefs.SetFloat($"{key}_g", color.g);
+            EditorPrefs.SetFloat($"{key}_b", color.b);
+            EditorPrefs.SetFloat($"{key}_a", color.a);
+        }
+
+        public static Color GetColor(string key)
+        {
+            Color defaultColor = Color.white;
+
+            if (EditorPrefs.HasKey($"{key}_r")
+                && EditorPrefs.HasKey($"{key}_g")
+                && EditorPrefs.HasKey($"{key}_b")
+                && EditorPrefs.HasKey($"{key}_a"))
+            {
+                return new Color(
+                    EditorPrefs.GetFloat($"{key}_r", defaultColor.r),
+                    EditorPrefs.GetFloat($"{key}_g", defaultColor.g),
+                    PlayerPrefs.GetFloat($"{key}_b", defaultColor.b),
+                    EditorPrefs.GetFloat($"{key}_a", defaultColor.a)
+                );
+            }
+            return defaultColor;
+        }
+
+        public static void SetObject(string key, object value)
         {
             EditorPrefs.SetString(key, JsonUtility.ToJson(value));
         }
 
-        public T GetObject<T>(string key)
+        public static T GetObject<T>(string key)
         {
             var json = EditorPrefs.GetString(key, string.Empty);
             if(string.IsNullOrEmpty(json))
@@ -24,6 +75,31 @@ namespace BrightTooling
             }
             var result = JsonUtility.FromJson<T>(json);
             return result;
+        }
+
+        public static void SetArray(string key, object value)
+        {
+            SetObject(key, value);
+        }
+
+        public static T[] GetArray<T>(string key)
+        {
+            string arrayData = EditorPrefs.GetString(key);
+
+            if (string.IsNullOrEmpty(arrayData))
+            {
+                return new T[0];
+            }
+
+            string[] stringArray = arrayData.Split(',');
+            T[] array = new T[stringArray.Length];
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                array[i] = (T)System.Convert.ChangeType(stringArray[i], typeof(T));
+            }
+
+            return array;
         }
     }
 }
